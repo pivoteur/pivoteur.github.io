e419c637be50080e7a7060247ae944b73cf78df6
pivoteur <dauclair@hotmail.com>
2025-02-27 16:46:30 -0500

    Functions calling functions: miraculous! ... and in BASH no less

--- env.sh
+++ env.sh
@@ -2,10 +2,8 @@ export PIVOT_DATA_DIR=$PIVOT_DIR/data
 
 export WALLETS=$PIVOT_DATA_DIR/wallets.tsv
 
-function update {
-   FILE=$1; DIR=data; TSV=$DIR/$FILE.tsv; rm $TSV; vi $TSV; git add $TSV
-}
+function update1 { TSV=$1.tsv; rm -f $TSV; vi $TSV; git add $TSV }
 
-function pool {
-   FILE=$1; DIR=data/pools; TSV=$DIR/$FILE.tsv; rm $TSV; vi $TSV; git add $TSV
-}
+function update { update1 data/$1 }
+
+function pool { update1 data/pools/$1 }
