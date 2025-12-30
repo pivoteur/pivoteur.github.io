69444c09962352f48174ad534b0a5c0e1ab64bb3
pivoteur <dauclair@hotmail.com>
2025-12-30 09:50:03 -0500

    simplified editing git files and git data files

--- env.sh
+++ env.sh
@@ -2,7 +2,11 @@ export PIVOT_DATA_DIR=$PIVOT_DIR/data
 
 export WALLETS=$PIVOT_DATA_DIR/wallets.tsv
 
-function update1 { TSV=$1.tsv; rm -f $TSV; vi $TSV; git add $TSV }
+# -- GIT -------------------------------------------------------------
+
+function vig { vi $1; git add $1 }
+
+function update1 { TSV=$1.tsv; rm -f $TSV; vig $TSV }
 
 function update { update1 data/$1 }
 
@@ -11,6 +15,15 @@ function opn { update1 data/pivots/open/raw/$1 }
 function cls { update1 data/pivots/close/raw/$1 }
 function stk { update1 data/pivots/stakes/raw/$1 }
 function dist { update1 data/pivots/dists/raw/$1 }
+
+# ... and a li'l bit o' obscuation
+
 function profile { hashed=$(printf "%s" "$1" | sha256); update1 data/profiles/$hashed }
+
+# ... editing files in-place without deleting the original
+
+function vid { vig data/$1.tsv }
+
+# -- REPORTAGE -------------------------------------------------------
 
 function gains { echo "* actual ROI: $1 / $2 APR projected" | despace }
