1a2393347076f2afe432c21190525da75cf9a9d1
pivoteur <dauclair@hotmail.com>
2026-04-18 13:23:26 -0400

    removed Cardano pools, 2026-04-18

--- libs/pool-assets.js
+++ libs/pool-assets.js
@@ -1,18 +1,14 @@
-
 // created by: pools, version: 1.00
 
 
 const poolAssets = {
-   generated: '2026-04-03',
+   generated: '2026-04-18',
    assets: [
-      [ 'ADA', 'USDC' ],
       [ 'AVAX', 'UNDEAD' ],
-      [ 'BTC', 'ADA' ],
       [ 'BTC', 'AVAX' ],
       [ 'BTC', 'ETH' ],
       [ 'BTC', 'UNDEAD' ],
       [ 'BTC', 'USDC' ],
-      [ 'ETH', 'ADA' ],
       [ 'ETH', 'UNDEAD' ],
       [ 'UNDEAD', 'USDC' ]
    ]
