f2bce3f440877a84282b00c8d9be18174d703ada
pivoteur <pivot.technologies.llc@gmail.com>
2026-07-22 07:57:05 +0000

    hwaet 2026-07-22

--- libs/pool-health.js
+++ libs/pool-health.js
@@ -1,11 +1,11 @@
 const poolHealth = {
-   generated: '2026-07-21',
+   generated: '2026-07-22',
    pools: [
       { pool: "UNDEAD+USDC", available: '$0.6252' },
       { pool: "ETH+UNDEAD", available: '$3.51' },
+      { pool: "BTC+UNDEAD", available: '$4.80' },
       { pool: "BTC+ETH", available: '$5.75' },
-      { pool: "BTC+USDC", available: '$5.84' },
-      { pool: "BTC+UNDEAD", available: '$7.20' },
+      { pool: "BTC+USDC", available: '$9.65' },
       { pool: "AVAX+UNDEAD", available: '$51.09' },
       { pool: "BTC+AVAX", available: '$97.83' }
    ]
