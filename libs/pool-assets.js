771cdfdb225d73d9b3308c7d57d2daa8d147da2a
pivoteur <pivot.technologies.llc@gmail.com>
2026-09-24 10:31:21 +0000

    pools 2026-09-24

--- libs/pool-assets.js
+++ libs/pool-assets.js
@@ -4,13 +4,13 @@
 
  
 const poolAssets = {
-   generated: '2026-09-23',
+   generated: '2026-09-24',
    assets: [
-      [ 'BTC', 'ETH' ],
       [ 'BTC', 'UNDEAD' ],
-      [ 'UNDEAD', 'USDC' ],
-      [ 'BTC', 'USDC' ],
       [ 'ETH', 'UNDEAD' ],
+      [ 'BTC', 'USDC' ],
+      [ 'BTC', 'ETH' ],
+      [ 'UNDEAD', 'USDC' ],
       [ 'BTC', 'AVAX' ],
       [ 'AVAX', 'UNDEAD' ]
       ]
